6fda8e1a9664d90706016f9c013d101deab5b17b
michu1145 <michaljankowski1445@gmail.com>
2019-03-04 16:27:16 +0100

    Revert "Zmiany do tutorialu, wczesniej nie widzialem"
    This reverts commit 031d6174c433ac709d979600be08b203b2143c6b.

--- src/client/tutorial.cpp
+++ src/client/tutorial.cpp
@@ -15,16 +15,16 @@
 
 int initSDL() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cout << "Nie mozna zainicjowac SDLA: " << SDL_GetError() << std::endl;
+		std::cout << "Nie mozna zainicjowac SDLA: " <<  SDL_GetError()<< std::endl;
 		return -1;
 	}
-	if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) < 0) {
+	if (IMG_Init(IMG_INIT_JPG|IMG_INIT_PNG) < 0) {
 		std::cout << "Nie mozna zainicjowac SDL_image: " << IMG_GetError() << std::endl;
 		return -1;
 	}
 	return 0;
 }
-//test
+
 
 int main(int argc, char *argv[])
 {
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 		//utworzenie okna
 		window = SDL_CreateWindow("Simple Shooter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
-
+		
 		//"zawartosc" okna
 		context = SDL_GL_CreateContext(window);
 
@@ -51,16 +51,13 @@ int main(int argc, char *argv[])
 
 	}
 	glEnable(GL_DEPTH_TEST);
-	
-		//wyswietlane jako siatka glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//wyswietlane jako siatka glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-		Shader ourShader("vertex.vs", "fragment.fs");
+	Shader ourShader("vertex.vs", "fragment.fs");
 	Model ourModel("res/models/headphones_UVW.fbx");
 	Model pistolet("res/models/pistolet/pistolet.obj");
 
-		//wyswietlane jako siatka
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		float deltaTime = 0.0;
+	float deltaTime = 0.0;
 	float lastFrame = 0.0;
 	SDL_Event windowEvent;
 	SDL_SetRelativeMouseMode(SDL_TRUE);
@@ -79,8 +76,8 @@ int main(int argc, char *argv[])
 			}
 			switch (windowEvent.type) {
 			case SDL_KEYDOWN: {
-
-				switch (windowEvent.key.keysym.sym) {
+				
+				switch (windowEvent.key.keysym.sym ) {
 				case SDLK_w: {
 					nextMove[Camera::MOVE_FORWARD] = 1;
 					break;
@@ -106,22 +103,18 @@ int main(int argc, char *argv[])
 				switch (windowEvent.key.keysym.sym) {
 				case SDLK_w: {
 					nextMove[Camera::MOVE_FORWARD] = 0;
-					std::cout << "gora";
 					break;
 				}
 				case SDLK_s: {
 					nextMove[Camera::MOVE_BACKWARD] = 0;
-					std::cout << "dol";
 					break;
 				}
 				case SDLK_a: {
 					nextMove[Camera::STRAFE_LEFT] = 0;
-					std::cout << "lewo";
 					break;
 				}
 				case SDLK_d: {
-					nextMove[Camera::STRAFE_RIGHT] = 0;
-					std::cout << "prawo";
+					nextMove[Camera::STRAFE_RIGHT] = 0;;
 					break;
 				}
 				default: break;
@@ -139,9 +132,7 @@ int main(int argc, char *argv[])
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		kamera.moveCamera(nextMove, deltaTime);
 
-
-
-			ourShader.use();
+		ourShader.use();
 		glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 		glm::mat4 view = kamera.getViewMatrix();
 		//glm::mat4 view = glm::mat4(1.0f);
@@ -157,11 +148,11 @@ int main(int argc, char *argv[])
 		ourShader.setMat4("view", view);
 		model = glm::mat4(1.0f);
 		std::cout << kamera.cameraPos.x << " " << kamera.cameraPos.y << " " << kamera.cameraPos.z << std::endl;
-	
-			model = glm::rotate(model, (float)glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, (float)glm::radians(-90.0f), glm::vec3(0.0f,1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(0.03f, 0.03f, 0.03f));
 		ourShader.setMat4("model", model);
 		pistolet.Draw(ourShader);
+		SDL_GL_SwapWindow(window);
 	}
 
 	SDL_GL_DeleteContext(context);
