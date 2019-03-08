5f641d3bc2be364f3d638b9d857b5e6079dc0ac7
jakubklimek97 <jakukli853@student.polsl.pl>
2019-03-08 16:42:16 +0100

    Nowa struktura na przechowywanie wlasciwosci materialu

--- src/client/Mesh.h
+++ src/client/Mesh.h
@@ -25,16 +25,24 @@ struct Texture {
 	std::string type;
 	std::string path;
 };
-
+//some objects may use simpler material properties instead of ie specular maps
+struct MaterialProperties {
+	glm::vec3 ambient = glm::vec3(0.0f);
+	glm::vec3 diffuse = glm::vec3(0.0f);
+	glm::vec3 specular = glm::vec3(0.0f);
+	float shininess;
+};
 class Mesh {
 public:
+	
 	std::vector<Vertex> vertices;
 	std::vector<unsigned int> indices;
 	std::vector<Texture> textures;
-	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures, MaterialProperties materialProperties);
 	~Mesh();
 	void Draw(Shader shader);
 private:
+	MaterialProperties materialProperties;
 	unsigned int VAO, VBO, EBO;
 	void setupMesh();
 };
