92b0ea799b789483f6e686f0b189d52a1ecdf343
jakubklimek97 <jakukli853@student.polsl.pl>
2019-04-19 13:31:53 +0200

    Kamera chodzi zamiast latać

--- src/client/Camera.h
+++ src/client/Camera.h
@@ -20,6 +20,7 @@ public:
 	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 	glm::vec3 cameraRight;
+	glm::vec3 movementFront = glm::vec3(0.0f, 0.0f, 1.0f);
 	float yaw = -90.0f;
 	float pitch = 0.0f;
 	float sensitivity = 0.1f;
