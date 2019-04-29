aa197f3ae3aa15920f1cd206d251bfb8eafd34aa
jakubklimek97 <jakukli853@student.polsl.pl>
2019-04-29 12:44:31 +0200

    Domyślny destruktor dla klasy Shader

--- src/client/Shader.h
+++ src/client/Shader.h
@@ -12,6 +12,7 @@ class Shader
 public:
 	unsigned int ID;
 	Shader(const char* vertexPath, const char* fragmentPath);
+	~Shader() = default;
 	void use();
 	void setBool(const std::string &name, bool value) const;
 	void setInt(const std::string &name, int value) const;
