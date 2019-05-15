070b089db86df96c38f99fc50e16cece0c68776c
jakubklimek97 <jakukli853@student.polsl.pl>
2019-05-15 18:52:51 +0200

    Drewniana implementacja komunikacji po sieci

--- src/client/SceneGame.h
+++ src/client/SceneGame.h
@@ -10,6 +10,7 @@ public:
 	virtual void UnInitScene();
 	virtual void handleEvents(SDL_Event& e);
 	virtual void render();
+	Entity * player2; //na razie, pozniej trafi do private
 private: //zmienne uzywane przez scene (indywidualne)
 	float ostatniWystrzal = 0.0f;
 	bool quit = false;
@@ -17,6 +18,5 @@ private: //zmienne uzywane przez scene (indywidualne)
 	float lastFrame = 0.0f;
 	float currentFrame = 0.0f;
 	Camera::Movement nextMove;
-	Entity * player2;
 };
 
--- src/client/tutorial.cpp
+++ src/client/tutorial.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
 #include <SDL.h>
 #include <SDL_image.h>
 #include "SDL_net.h"
@@ -28,13 +29,14 @@
 std::mutex loopLock;
 std::mutex posLock;
 std::mutex playerPosLock;
-float pos[3];
-float playerPos[3];
+float pos[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+float playerPos[4] = { 0.0f };
+std::thread* connectionThread;
 
 void connectionHandlerThread() {
 	bool connection = true;
 	Networking::MessageType ctrl;
-	float posTemp[3];
+	float posTemp[4];
 	while (connection) {
 		Networking::recvControlMsg(&ctrl);
 		if (ctrl == Networking::MessageType::POSITION_FOLLOWS) {
@@ -44,26 +46,72 @@ void connectionHandlerThread() {
 				pos[0] = posTemp[0];
 				pos[1] = posTemp[1];
 				pos[2] = posTemp[2];
+				pos[3] = posTemp[3];
 				posLock.unlock();
 			}
 			else break;
 		}
 		else if (ctrl == Networking::MessageType::QUIT) {
-
+			std::cout << "Zakonczenie przez serwer" << std::endl;
+			Networking::closeConnection();
+			connection = false;
+			return; //zakonczenie polaczenia
 		}
 		Networking::sendControlMsg(Networking::MessageType::POSITION_FOLLOWS);
 		playerPosLock.lock();
 		posTemp[0] = playerPos[0];
 		posTemp[1] = playerPos[1];
 		posTemp[2] = playerPos[2];
+		posTemp[3] = playerPos[3];
 		playerPosLock.unlock();
 		Networking::sendData(posTemp);
-		SDL_Delay(16); //16ms opoznienia miedzy wiadomosciami
+		std::this_thread::sleep_for(std::chrono::milliseconds(16)); //16ms opoznienia miedzy wiadomosciami
 	}
 }
 void serverConnectionHandlerThread() {
-
-
+	bool connection = true;
+	Networking::MessageType ctrl;
+	float posTemp[4];
+	while (connection) {
+		Networking::sendControlMsg(Networking::MessageType::POSITION_FOLLOWS);
+		playerPosLock.lock();
+		posTemp[0] = playerPos[0];
+		posTemp[1] = playerPos[1];
+		posTemp[2] = playerPos[2];
+		posTemp[3] = playerPos[3];
+		playerPosLock.unlock();
+		Networking::sendData(posTemp);
+		Networking::recvControlMsg(&ctrl);
+		if (ctrl == Networking::MessageType::POSITION_FOLLOWS) {
+			connection = Networking::recvData(posTemp);
+			if (connection) {
+				posLock.lock();
+				pos[0] = posTemp[0];
+				pos[1] = posTemp[1];
+				pos[2] = posTemp[2];
+				pos[3] = posTemp[3];
+				posLock.unlock();
+			}
+			else break;
+		}
+		else if (ctrl == Networking::MessageType::QUIT) {
+			std::cout << "Zakonczenie przez serwer" << std::endl;
+			Networking::closeConnection();
+			connection = false;
+			break; //zakonczenie polaczenia
+		}	
+	}
+	connection = false;
+	Networking::closeConnection();
+}
+void serverThread() {
+	Networking::startServer(60100);
+	bool connection;
+	do {
+		SDL_Delay(1000);
+		connection = Networking::acceptConnection();
+		std::cout << "Dalej nic..." << std::endl;
+	} while (!connection);
 }
 
 int initSDL() {
@@ -101,6 +149,8 @@ void initOpenGL(SDL_Window* &pWindow, SDL_GLContext &context) {
 
 int main(int argc, char *argv[])
 {
+	int czySerwer; //1- serwer, 0 - klient
+	std::cin >> czySerwer;
 	if (initSDL() < 0) return -1;
 	int serwer = parseParams(argc, argv);
 	SDL_Window* window;
@@ -129,14 +179,43 @@ int main(int argc, char *argv[])
 	SceneGame* scenaGra = new SceneGame(glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.01f, 100.0f), new Camera(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), 0.1f, 8.0f));
 	scenaGra->InitScene();
 	SDL_Event e;
+	Networking::setDataSize(sizeof(pos));
+	//prosta obsluga serwera(na razie w mainie)
+	if (czySerwer) {
+		serverThread();
+		connectionThread = new std::thread(serverConnectionHandlerThread);
+	}
+	else {
+		bool connection = Networking::connect("localhost", 60100);
+		if (connection) {
+			connectionThread = new std::thread(connectionHandlerThread);
+		}
+	}
+	/////////////////////////////////////////////////
 	while (scenaGra->run)
 	{
 		scenaGra->handleEvents(e);
+		glm::vec3 playerPosV = scenaGra->getCamera()->cameraPos;
+		playerPosLock.lock();
+		playerPos[0] = playerPosV.x;
+		playerPos[1] = playerPosV.y-1.0f;
+		playerPos[2] = playerPosV.z;
+		playerPos[3] = scenaGra->getCamera()->yaw;
+		playerPosLock.unlock();
+		posLock.lock();
+		glm::vec3 player2pos(pos[0], pos[1], pos[2]);
+		scenaGra->player2->setRotationY(-glm::radians(pos[3]));
+		posLock.unlock();
+		scenaGra->player2->setPosition(player2pos);
+		//scenaGra->player2->setRotationY(-glm::radians(scenaGra->getCamera()->yaw));
 		scenaGra->render();
-			
+		std::cout << pos[3] << std::endl;
 		
 		SDL_GL_SwapWindow(window); // zostanie w glownej petli aplikacji, nie ma sensu sie powtarzac
 	}
+	//na razie takie zakonczenie serwera
+	Networking::stopServer();
+	////////////////////////////
 	scenaGra->UnInitScene();
 	delete scenaGra;
 	Loader::unloadShaders();
